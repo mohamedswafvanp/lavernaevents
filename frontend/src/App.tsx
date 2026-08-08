56ab7d181d04ed27896b56276616a7159d612f72
mohamedswafvanp <pmuthusafwan@gmail.com>
2026-08-08 10:25:59 +0530

    Public Files Created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,19 @@
 
+import PublicLayout from "./layouts/PublicLayout"
+import Footer from "./components/public/Footer"
+import Navbar from "./components/public/Navbar"
+import Home from "./pages/public/Home"
+
 function App() {
-	return <main />
+	return (
+		<PublicLayout>
+			<Navbar />
+			<main>
+				<Home />
+			</main>
+			<Footer />
+		</PublicLayout>
+	)
 }
 
 export default App
